f99f4e1f7e7738536b406ecd461f1abc414e5e0f
Maarten <maartenhandels@gmail.com>
2023-10-15 13:05:52 +0100

    Improve containers

--- src/components/UI/SectionTitle/SectionTitle.js
+++ src/components/UI/SectionTitle/SectionTitle.js
@@ -2,12 +2,12 @@ import React from "react";
 
 import classes from "./SectionTitle.module.css";
 
-const SectionTitle = ({subtitle, title}) => {
+const SectionTitle = ({ subtitle, title, className }) => {
     return (
-        <>
+        <div className={className}>
             <h3 className={classes.SectionSubtitle}>{subtitle}</h3>
             <h1 className={classes.SectionTitle}>{title}</h1>
-        </>
+        </div>
     )
 }
 
