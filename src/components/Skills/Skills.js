1f0ab0419444de3897b2200a11c798b1f8492c63
Maarten <maartenhandels@gmail.com>
2023-10-15 22:35:49 +0100

    Add content to skills page

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -7,6 +7,7 @@ import Skill from "./Skill/Skill";
 
 import {faJava} from "@fortawesome/fontawesome-free-brands";
 import {faPython} from "@fortawesome/fontawesome-free-brands";
+import {faJsSquare} from "@fortawesome/fontawesome-free-brands";
 import {faReact} from "@fortawesome/fontawesome-free-brands";
 import {faPeopleLine} from "@fortawesome/free-solid-svg-icons";
 import SectionTitle from "../UI/SectionTitle/SectionTitle";
@@ -14,22 +15,41 @@ import ColumnsContainer from "../../containers/ColumnsContainer/ColumnsContainer
 
 const Skills = ({ skills }) => {
 
-    const skillsList = [
+    const technicalSkills = [
         { icon: faJava, name: 'Java' },
         { icon: faPython, name: 'Python' },
+        { icon: faJsSquare, name: 'Javascript' },
+        { src: process.env.PUBLIC_URL + "/images/spring-boot-icon.png", name: 'Spring' },
         { icon: faReact, name: 'React'},
-        { icon: faPeopleLine, name: 'Team Work' },
-        { src: process.env.PUBLIC_URL + "/images/spring-boot-logo.png", name: 'Spring' },
-        { icon: faJava, name: 'React'},
+        { src: process.env.PUBLIC_URL + "/images/microsoft-icon.png", name: 'Azure'},
+        { src: process.env.PUBLIC_URL + "/images/docker-icon.png", name: 'Docker'},
+        { src: process.env.PUBLIC_URL + "/images/kubernetes-icon.png", name: 'Kubernetes'},
+        { src: process.env.PUBLIC_URL + "/images/kafka-icon.png", name: 'Kafka'},
+
     ];
+
+    const softSkills = [
+        { src: process.env.PUBLIC_URL + "/images/scrum-icon.png", name: 'Scrum' },
+        { icon: faPeopleLine, name: 'Team Work' },
+        { src: process.env.PUBLIC_URL + "/images/leader-icon.png", name: 'Leadership' },
+    ]
 
     return (
         <ColumnsContainer>
             <SectionTitle title="Skills" subtitle="🪚 Professional Skills" className={classes.SkillsTitle}/>
             <div className={classes.SkillsList}>
-                {skillsList.map((skill, index) => (
-                    <Skill key={index} skill={skill.name} icon={skill.icon} src={skill.src}/>
-                ))}
+                <SectionTitle subtitle="Technical Skills" />
+                <div className={classes.SkillsGroup}>
+                    {technicalSkills.map((skill, index) => (
+                        <Skill key={index} skill={skill.name} icon={skill.icon} src={skill.src}/>
+                    ))}
+                </div>
+                <SectionTitle subtitle="Soft Skills" />
+                <div className={classes.SkillsGroup}>
+                    {softSkills.map((skill, index) => (
+                        <Skill key={index} skill={skill.name} icon={skill.icon} src={skill.src}/>
+                    ))}
+                </div>
             </div>
         </ColumnsContainer>
     );
